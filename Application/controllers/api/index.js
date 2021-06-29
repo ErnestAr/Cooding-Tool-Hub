4ed90f0b602dbe5f3e0de74846fff434f0b75ade
ErnestAr <arutiunian.ernest@gmail.com>
2021-06-29 14:39:52 -0400

    home page and accoun

--- Application/controllers/api/index.js
+++ Application/controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
+const techRoutes = require('./newtech-routes');
 
 router.use('/users', userRoutes);
+router.use('/newtech', techRoutes);
 
 module.exports = router;
