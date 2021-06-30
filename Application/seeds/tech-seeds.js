be131b00f902d01a4c4dfdad131d83e887fe4aa7
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-06-30 10:37:59 -0400

    Added and updated seeds, js

--- Application/seeds/tech-seeds.js
+++ Application/seeds/tech-seeds.js
@@ -2,7 +2,7 @@ const { Tech } = require('../models');
 
 const techdata = [
   {
-    name: 'Maya',
+    tech_name: 'Maya',
     description: 
     "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
     project: "Disney Short",
@@ -10,63 +10,15 @@ const techdata = [
     user_id: 1,
   },
   {
-    name: 'Maya',
+    tech_name: 'MySQL',
     description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
+    "MySQL is an Oracle-backed open source relational database management system (RDBMS) based on Structured Query Language (SQL).",
+    project: "E-Commerce Website",
+    category_id: 4,
+    user_id: 2,
   },
   {
-    name: 'Maya',
-    description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
-  },
-  {
-    name: 'Maya',
-    description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
-  },
-  {
-    name: 'Maya',
-    description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
-  },
-  {
-    name: 'Maya',
-    description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
-  },
-  {
-    name: 'Maya',
-    description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
-  },
-  {
-    name: 'Maya',
-    description: 
-    "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
-    project: "Disney Short",
-    category_id: 1,
-    user_id: 1,
-  },
-  {
-    name: 'Maya',
+    tech_name: 'Bootstrap',
     description: 
     "Maya is an animation and modeling program used to create three-dimensional ( 3-D ), full-motion effects. Maya incorporates the natural laws of physics to control the behavior of virtual objects in computer animation. Maya can produce videos that are more life-like than has been possible with less sophisticated programs. Versions are available for both IBM-compatible and Macintosh operating systems.",
     project: "Disney Short",
