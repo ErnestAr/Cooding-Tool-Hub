f3c27c6993097f15a4c34551463e5ea1e3fd769c
andresaponte22 <80225604+andresaponte22@users.noreply.github.com>
2021-07-01 14:43:58 -0400

    added addlikes logic

---
+++ Application/public/js/addlike.js
@@ -0,0 +1,8 @@
+const addLike = async (event) => {
+  event.preventDefault()
+  // placeholder
+  const techId = document.getElementById('tech-card')
+  const elId = dataset.techId
+
+  const response = await fetch('')
+}
