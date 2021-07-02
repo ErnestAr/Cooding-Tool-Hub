e3a492fd1f32b97f33a6c407d38640823af63d2c
ErnestAr <arutiunian.ernest@gmail.com>
2021-07-02 16:51:03 -0400

    logout, log in and sign up working properly

--- Application/public/js/login.js
+++ Application/public/js/login.js
@@ -14,9 +14,9 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/category');
     } else {
-      alert('Failed to log in.');
+      document.location.replace('/login');
     }
   }
 };
